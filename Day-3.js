670f7b3fcf835c36f5c61d1a6095101d35a1ef16
ShivaSingh17121997 <shivasingh001997@gmail.com>
2024-07-31 14:20:58 +0530

    added

---
+++ Day-3.js
@@ -0,0 +1,66 @@
+// variables   var hello()
+
+
+var a = 10;   // global scope
+
+var Name = "Radhe shyam "
+
+console.log(Name)
+
+
+var obj = { name: "ram", age: 21, city: "ayodhya" }
+console.log(obj)
+
+
+// types of varibales ES = 3 taypes = var , let , const
+
+// const 
+const age = 21
+console.log("age", age)
+
+
+// let 
+let userName = "Ram"
+
+userName = "Ram chandra"
+
+userName = "Raja Ram chandra "
+
+console.log(userName);
+
+
+let city
+city = "surat"
+console.log(city)
+
+
+// var
+
+var hobby = "guiter"
+
+hobby = "tabala"
+
+console.log(hobby)
+
+
+skills = "coding"
+var skills
+console.log(skills);
+
+// hoisting  var 
+
+// hoisting is very important
+
+function hello() {
+    // block scope // local scope
+}
+
+hello() // function calling
+
+//
+
+
+// data types
+
+// primitive number, boolean (true, false) , string(" ") , symobie(!@#$%^&*()), null, undefined
+// non primitive values array = [], obj={},
