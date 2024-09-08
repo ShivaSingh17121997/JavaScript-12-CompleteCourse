e2afe09d560bfbcddeed28f0ec4402fae696c2d3
ShivaSingh17121997 <shivasingh001997@gmail.com>
2024-09-08 19:12:23 +0530

    added the file

--- Day7.js/closures.js
+++ Day7.js/closures.js
@@ -1,124 +1,124 @@
-// function counter() {
-//     let count = 0;
+// // function counter() {
+// //     let count = 0;
 
-//     return {
-//         increment: function () {
-//             count += 1
-//             return count
-//         },
-//         decrement: function () {
-//             count -= 1
-//             return count
-//         },
-//         getCount: function () {
-//             return count
-//         }
-//     }
-// }
+// //     return {
+// //         increment: function () {
+// //             count += 1
+// //             return count
+// //         },
+// //         decrement: function () {
+// //             count -= 1
+// //             return count
+// //         },
+// //         getCount: function () {
+// //             return count
+// //         }
+// //     }
+// // }
 
-// let myFun = counter()
+// // let myFun = counter()
 
-// console.log(myFun.increment())
-// console.log(myFun.increment())
-// console.log(myFun.increment())
-// console.log(myFun.decrement())
-// console.log(myFun.getCount())
-// console.log(myFun.count)
-
-
-// (function(){console.log("hello")})()
-
-//  
-
-// function factorial(n) {
-
-//     if (n == 0 || n == 1) {
-//         return 1
-//     }
-
-//     return n * factorial(n - 1)
-
-// }
-
-// console.log(factorial(5))
-
-const memo = {};
-
-// function fibonaci(n) {
-//     if (n == 0 || n == 1) {
-//         return n
-//     }
-
-//     if (memo[n]) {
-//         return memo[n]
-//     }
-
-//     // recursive case
-//     memo[n] = fibonaci(n - 1) + fibonaci(n - 2)
-//     return memo[n]
-// }
+// // console.log(myFun.increment())
+// // console.log(myFun.increment())
+// // console.log(myFun.increment())
+// // console.log(myFun.decrement())
+// // console.log(myFun.getCount())
+// // console.log(myFun.count)
 
 
-// console.log(fibonaci(10))
+// // (function(){console.log("hello")})()
+
+// //  
+
+// // function factorial(n) {
+
+// //     if (n == 0 || n == 1) {
+// //         return 1
+// //     }
+
+// //     return n * factorial(n - 1)
+
+// // }
+
+// // console.log(factorial(5))
+
+// const memo = {};
+
+// // function fibonaci(n) {
+// //     if (n == 0 || n == 1) {
+// //         return n
+// //     }
+
+// //     if (memo[n]) {
+// //         return memo[n]
+// //     }
+
+// //     // recursive case
+// //     memo[n] = fibonaci(n - 1) + fibonaci(n - 2)
+// //     return memo[n]
+// // }
 
 
+// // console.log(fibonaci(10))
 
 
 
-// currying
 
+
+// // currying
+
+
+// // function sum(...args) {
+// //     let arr = [...args]
+// //     function add(b) {
+// //         return arr.reduce((acc, cv) => { return acc + cv }, 0) + b
+// //     }
+// //     return add;
+// // }
+
+// // console.log(sum(2, 3, 4, 5, 6)(3));
+
+
+
+// // 2 
+
+// // function sum(a) {
+
+// //     let arr = [a];
+// //     console.log(arr)
+
+// //     function add(b) {
+
+// //         if ( typeof b === "undefined") {
+// //             return arr.reduce((acc, cv) => { return acc + cv },0)
+// //         } else {
+// //             arr.push(b)
+// //             return add
+// //         }
+
+// //     }
+// //     return add
+// // }
+
+
+// // console.log(sum(3)(4)(5)(6)(7)(7)(9)(10)()); 
+
+
+
+// //3
 
 // function sum(...args) {
-//     let arr = [...args]
-//     function add(b) {
-//         return arr.reduce((acc, cv) => { return acc + cv }, 0) + b
-//     }
-//     return add;
-// }
+//     // let arr = [...args]
+//     // console.log(arr)
+//     let total = args.reduce((acc, cv) => acc + cv, 0);
 
-// console.log(sum(2, 3, 4, 5, 6)(3));
-
-
-
-// 2 
-
-// function sum(a) {
-
-//     let arr = [a];
-//     console.log(arr)
-
-//     function add(b) {
-
-//         if ( typeof b === "undefined") {
-//             return arr.reduce((acc, cv) => { return acc + cv },0)
-//         } else {
-//             arr.push(b)
-//             return add
-//         }
+//     function add(...args) {
+//         console.log(args)
+//         return total += args.reduce((acc, cv) => { return acc + cv }, 0)
 
 //     }
 //     return add
 // }
 
 
-// console.log(sum(3)(4)(5)(6)(7)(7)(9)(10)()); 
-
-
-
-//3
-
-function sum(...args) {
-    // let arr = [...args]
-    // console.log(arr)
-    let total = args.reduce((acc, cv) => acc + cv, 0);
-
-    function add(...args) {
-        console.log(args)
-        return total += args.reduce((acc, cv) => { return acc + cv }, 0)
-
-    }
-    return add
-}
-
-
-console.log(sum(1, 3, 4, 5)(3, 4, 5, 6, 6))
+// console.log(sum(1, 3, 4, 5)(3, 4, 5, 6, 6))
--- Day7.js/polyfills.js
+++ Day7.js/polyfills.js
@@ -2,249 +2,249 @@
 
 
 
-// console.log((typeof Array.prototype))
+console.log((typeof Array.prototype))
 
-// polyfill of map
-// Array.prototype.myMapFun = function (callback) {
-//     if (!callback) {
-//         throw Error("undefined is not a function")
-//     }
-//     let arr = this;
-//     console.log(arr)
-//     let newArr = [];
+polyfill of map
+Array.prototype.myMapFun = function (callback) {
+    if (!callback) {
+        throw Error("undefined is not a function")
+    }
+    let arr = this;
+    console.log(arr)
+    let newArr = [];
 
-//     for (let i = 0; i < arr.length; i++) {
-//         const result = callback(arr[i], i, arr)
-//         newArr.push(result)
-//     }
-//     return newArr;
-// }
+    for (let i = 0; i < arr.length; i++) {
+        const result = callback(arr[i], i, arr)
+        newArr.push(result)
+    }
+    return newArr;
+}
 
-// Array.prototype.myMapFun(() => { })
+Array.prototype.myMapFun(() => { })
 
 
-// let arrr = [1, 2, 3, 4, 5]
+let arrr = [1, 2, 3, 4, 5]
 
-// let maparr = arrr.myMapFun((item) => {
-//     console.log(item * 2)
-// })
+let maparr = arrr.myMapFun((item) => {
+    console.log(item * 2)
+})
 
 
 
-// filtet polyfill
-// Array.prototype.myFilterFun = function (callback) {
+filtet polyfill
+Array.prototype.myFilterFun = function (callback) {
 
-//     if (typeof callback !== "function") throw Error("Undefined is not a function ")
+    if (typeof callback !== "function") throw Error("Undefined is not a function ")
 
-//     let arr = this;
-//     let newArr = [];
+    let arr = this;
+    let newArr = [];
 
-//     for (let i = 0; i < arr.length; i++) {
-//         let result = callback(arr[i], i, arr)
+    for (let i = 0; i < arr.length; i++) {
+        let result = callback(arr[i], i, arr)
 
-//         if (result) {
-//             newArr.push(arr[i]);
-//         }
-//     }
-//     return newArr;
+        if (result) {
+            newArr.push(arr[i]);
+        }
+    }
+    return newArr;
 
-// }
+}
 
-// let filArr = [1, 2, 3, 4, 5, 6]
-// Array.prototype.myFilterFun(() => { })
+let filArr = [1, 2, 3, 4, 5, 6]
+Array.prototype.myFilterFun(() => { })
 
-// let newFil = filArr.myFilterFun((item) => {
-//     return item < 4
-// })
+let newFil = filArr.myFilterFun((item) => {
+    return item < 4
+})
 
-// console.log(newFil)
+console.log(newFil)
 
 
-// polyfilof reduce
+polyfilof reduce
 
-// Array.prototype.myReduce = function (callback, IntialVal) {
-//     if (typeof callback !== "function") throw Error("undefined is not a function!");
+Array.prototype.myReduce = function (callback, IntialVal) {
+    if (typeof callback !== "function") throw Error("undefined is not a function!");
 
-//     let arr = this
-//     for (let i = 0; i < arr.length; i++) {
-//         const ans = callback(IntialVal, arr[i], i, arr)
-//         IntialVal = ans;
-//     }
-//     return IntialVal
-// }
+    let arr = this
+    for (let i = 0; i < arr.length; i++) {
+        const ans = callback(IntialVal, arr[i], i, arr)
+        IntialVal = ans;
+    }
+    return IntialVal
+}
 
 
-// let arr = [1, 2, 3, 4, 5]
+let arr = [1, 2, 3, 4, 5]
 
-// let val = arr.myReduce((acc, cv) => {
-//     return acc + cv
-// }, 0)
+let val = arr.myReduce((acc, cv) => {
+    return acc + cv
+}, 0)
 
-// console.log(val)
+console.log(val)
 
-// let arr = [3, 4, 5, [3, 4, 3], 9, 3]
+let arr = [3, 4, 5, [3, 4, 3], 9, 3]
 
-//  console.log(arr.flat())
+ console.log(arr.flat())
 
-// Array.prototype.myFlat = function () {
+Array.prototype.myFlat = function () {
 
-//     const arr = this;
-//     const result = [];
+    const arr = this;
+    const result = [];
 
-//     function flat(arr) {
-//         for (let i = 0; i < arr.length; i++) {
-//             if (Array.isArray(arr[i])) {
-//                 flat(arr[i])
-//             } else {
-//                 result.push(arr[i]);
-//             }
-//         }
-//     }
-//     flat(arr)
+    function flat(arr) {
+        for (let i = 0; i < arr.length; i++) {
+            if (Array.isArray(arr[i])) {
+                flat(arr[i])
+            } else {
+                result.push(arr[i]);
+            }
+        }
+    }
+    flat(arr)
 
-//     return result;
-// }
+    return result;
+}
 
-// let flattenArray = arr.myFlat()
-// console.log(flattenArray)
+let flattenArray = arr.myFlat()
+console.log(flattenArray)
 
 
 
 
-// practice question of polyfills
-// polyfill of map functin 
+practice question of polyfills
+polyfill of map functin 
 
-// let arr = [1, 2, 3, 4, 5]
+let arr = [1, 2, 3, 4, 5]
 
-// Array.prototype.myMapFunction = function (callback) {
+Array.prototype.myMapFunction = function (callback) {
 
-//     if (typeof callback !== "function") throw Error("Undefined is not a function ");
+    if (typeof callback !== "function") throw Error("Undefined is not a function ");
 
-//     let newArr = [];
-//     let arr = this;
+    let newArr = [];
+    let arr = this;
 
-//     for (let i = 0; i < arr.length; i++) {
-//         let res = callback(arr[i], i, arr)
-//         newArr.push(res);
-//     }
+    for (let i = 0; i < arr.length; i++) {
+        let res = callback(arr[i], i, arr)
+        newArr.push(res);
+    }
 
-//     return newArr;
+    return newArr;
 
-// }
+}
 
-// let answer = arr.myMapFunction((item) => item * 4)
-// console.log(answer)
+let answer = arr.myMapFunction((item) => item * 4)
+console.log(answer)
 
 
-// polyfill of filter 
+polyfill of filter 
 
-// let arr = [1, 2, 3, 4, 5];
+let arr = [1, 2, 3, 4, 5];
 
-// Array.prototype.myFilterFun = function (callback) {
+Array.prototype.myFilterFun = function (callback) {
 
-//     if (typeof callback !== 'function') throw Error("undefined is not a function");
-//     let arr = this;
-//     let newArr = [];
+    if (typeof callback !== 'function') throw Error("undefined is not a function");
+    let arr = this;
+    let newArr = [];
 
-//     for (let i = 0; i < arr.length; i++) {
-//         let ans = callback(arr[i], i, arr)
-//         if (ans) {
-//             newArr.push(arr[i])
-//         }
-//     }
-//     return newArr;
-// }
+    for (let i = 0; i < arr.length; i++) {
+        let ans = callback(arr[i], i, arr)
+        if (ans) {
+            newArr.push(arr[i])
+        }
+    }
+    return newArr;
+}
 
-// let filteredArr = arr.myFilterFun((item) => { return item == 4 });
-// console.log(filteredArr)
+let filteredArr = arr.myFilterFun((item) => { return item == 4 });
+console.log(filteredArr)
 
 
 
-// polyfill of reduce function 
+polyfill of reduce function 
 
-// let arr = [1, 2, 3, 4, 5];
-// Array.prototype.myReduceFun = function (callback, initialVal) {
-//     if (typeof callback !== 'function') {
-//         throw Error("Undefined is not a function");
-//     }
+let arr = [1, 2, 3, 4, 5];
+Array.prototype.myReduceFun = function (callback, initialVal) {
+    if (typeof callback !== 'function') {
+        throw Error("Undefined is not a function");
+    }
 
-//     let arr = this;
+    let arr = this;
 
 
-//     for (let i = 0; i < arr.length; i++) {
-//         let ans = callback(initialVal, arr[i], i, arr)
-//         initialVal = ans;
-//     }
-//     return initialVal;
-// }
+    for (let i = 0; i < arr.length; i++) {
+        let ans = callback(initialVal, arr[i], i, arr)
+        initialVal = ans;
+    }
+    return initialVal;
+}
 
-// let val = arr.myReduceFun((acc, cv) => {
-//     console.log("acc", acc);
-//     return acc + cv;
-// }, 0);
-// console.log(val);
+let val = arr.myReduceFun((acc, cv) => {
+    console.log("acc", acc);
+    return acc + cv;
+}, 0);
+console.log(val);
 
 
-// polyfill of flat array;
+polyfill of flat array;
 
-// let array = [1, 2, 3, [4, 5, [3, 4], 6, [7, 8, 9], 8], 3, 4, 5];
+let array = [1, 2, 3, [4, 5, [3, 4], 6, [7, 8, 9], 8], 3, 4, 5];
 
-// Array.prototype.myflatFun = function () {
-//     let newArr = [];
-//     let arr = this;
+Array.prototype.myflatFun = function () {
+    let newArr = [];
+    let arr = this;
 
-//     function flat(arr) {
-//         for (let i = 0; i < arr.length; i++) {
-//             if (Array.isArray(arr[i])) {
-//                 // console.log(arr[i])
-//                  flat(arr[i]);
-//             }
-//             else newArr.push(arr[i])
-//         }
-//     }
-//     flat(arr);
-//     return newArr;
-// }
+    function flat(arr) {
+        for (let i = 0; i < arr.length; i++) {
+            if (Array.isArray(arr[i])) {
+                // console.log(arr[i])
+                 flat(arr[i]);
+            }
+            else newArr.push(arr[i])
+        }
+    }
+    flat(arr);
+    return newArr;
+}
 
-// console.log(array.myflatFun());
+console.log(array.myflatFun());
 
 
 
-// polyfil of call , apply
+polyfil of call , apply
 
-// Function.prototype.mybindFun = function (context = {}, args = []) {
-//     if (typeof this !== 'function') throw Error("This is not a callable function ")
-//     if (!Array.isArray(args)) throw Error("argument should be array")
+Function.prototype.mybindFun = function (context = {}, args = []) {
+    if (typeof this !== 'function') throw Error("This is not a callable function ")
+    if (!Array.isArray(args)) throw Error("argument should be array")
 
-//     context.fn = this
-//     context.fn(...args)
-// }
+    context.fn = this
+    context.fn(...args)
+}
 
-// let obj = {
-//     name: "ram",
-//     age: "12"
-// }
+let obj = {
+    name: "ram",
+    age: "12"
+}
 
-// function hello() {
-//     console.log(`hello ${this.name} my age is ${this.age}`)
-// }
+function hello() {
+    console.log(`hello ${this.name} my age is ${this.age}`)
+}
 
-// 
 
 
-//    polyfill for bind function 
 
-// Function.prototype.myBindFun = function (contex = {}, ...args) {
-//     if (typeof this !== "function") {
-//         throw Error("this is not a function ")
-//     }
+   polyfill for bind function 
 
-//     contex.fn = this;
-//     return function () {
-//         return contex.fn(...args)
-//     }
+Function.prototype.myBindFun = function (contex = {}, ...args) {
+    if (typeof this !== "function") {
+        throw Error("this is not a function ")
+    }
 
-// }
+    contex.fn = this;
+    return function () {
+        return contex.fn(...args)
+    }
 
-// let newufn = hello.myBindFun(obj, 12)
-// console.log(newufn())
+}
+
+let newufn = hello.myBindFun(obj, 12)
+console.log(newufn())
